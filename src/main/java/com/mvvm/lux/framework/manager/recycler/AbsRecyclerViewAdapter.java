99aeebda04a631833b0e3e734745e8924b79593a
Luthor <luxiao418@pingan.com.cn>
2017-03-01 14:21:18 +0800

    优化AbsrecycleView

--- src/main/java/com/mvvm/lux/framework/manager/recycler/AbsRecyclerViewAdapter.java
+++ src/main/java/com/mvvm/lux/framework/manager/recycler/AbsRecyclerViewAdapter.java
@@ -3,7 +3,9 @@ package com.mvvm.lux.framework.manager.recycler;
 import android.content.Context;
 import android.support.annotation.IdRes;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,20 +16,25 @@ import java.util.List;
  * <p/>
  * RecycleViewAdapter基类
  */
-public abstract class AbsRecyclerViewAdapter extends
+public abstract class AbsRecyclerViewAdapter<T> extends
         RecyclerView.Adapter<AbsRecyclerViewAdapter.ClickableViewHolder> {
 
-
+    private List<T> datas;
+    private int resLayout;
     private Context context;
-
-    protected RecyclerView mRecyclerView;
+    private OnItemClickListener itemClickListener;
+    private OnItemLongClickListener itemLongClickListener;
 
     private List<RecyclerView.OnScrollListener> mListeners = new ArrayList<>();
 
-    public AbsRecyclerViewAdapter(RecyclerView recyclerView) {
+    public AbsRecyclerViewAdapter(RecyclerView recyclerView, @IdRes int resLayout, List<T> datas) {
+        this.resLayout = resLayout;
+        this.datas = datas;
+        initListener(recyclerView);
+    }
 
-        this.mRecyclerView = recyclerView;
-        this.mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+    private void initListener(RecyclerView recyclerView) {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
 
             @Override
             public void onScrollStateChanged(RecyclerView rv, int newState) {
@@ -47,48 +54,9 @@ public abstract class AbsRecyclerViewAdapter extends
         });
     }
 
-    public void addOnScrollListener(RecyclerView.OnScrollListener listener) {
-
-        mListeners.add(listener);
-    }
-
-    public interface OnItemClickListener {
-
-        public void onItemClick(int position, ClickableViewHolder holder);
-    }
-
-    interface OnItemLongClickListener {
-
-        boolean onItemLongClick(int position, ClickableViewHolder holder);
-    }
-
-    private OnItemClickListener itemClickListener;
-
-    private OnItemLongClickListener itemLongClickListener;
-
-    public void setOnItemClickListener(OnItemClickListener listener) {
-
-        this.itemClickListener = listener;
-    }
-
-    public void setOnItemLongClickListener(OnItemLongClickListener listener) {
-
-        this.itemLongClickListener = listener;
-    }
-
-    public void bindContext(Context context) {
-
-        this.context = context;
-    }
-
-    public Context getContext() {
-
-        return this.context;
-    }
-
     @Override
     public void onBindViewHolder(final ClickableViewHolder holder, final int position) {
-
+        convert(holder, datas.get(position), position);
         holder.getParentView().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -106,23 +74,61 @@ public abstract class AbsRecyclerViewAdapter extends
         });
     }
 
-    public class ClickableViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public ClickableViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(getContext()).inflate(resLayout, parent, false);
+        return new ClickableViewHolder(view);
+    }
+
+    public abstract void convert(ClickableViewHolder holder, T t, int position);
+
+    @Override
+    public int getItemCount() {
+        return datas == null ? 0 : datas.size();
+    }
+
+    public void addOnScrollListener(RecyclerView.OnScrollListener listener) {
+        mListeners.add(listener);
+    }
+
+    public interface OnItemClickListener {
+        public void onItemClick(int position, ClickableViewHolder holder);
+    }
+
+    interface OnItemLongClickListener {
+        boolean onItemLongClick(int position, ClickableViewHolder holder);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.itemClickListener = listener;
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener listener) {
+        this.itemLongClickListener = listener;
+    }
+
+    public void bindContext(Context context) {
+        this.context = context;
+    }
+
+    public Context getContext() {
+        return this.context;
+    }
+
+    public static class ClickableViewHolder extends RecyclerView.ViewHolder {
 
         private View parentView;
 
         public ClickableViewHolder(View itemView) {
-
             super(itemView);
             this.parentView = itemView;
         }
 
         View getParentView() {
-
             return parentView;
         }
 
         public <T extends View> T $(@IdRes int id) {
-
             return (T) parentView.findViewById(id);
         }
     }
