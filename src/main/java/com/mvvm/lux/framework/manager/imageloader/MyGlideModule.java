6595096d1d243bfa492faabcbf4465fb16f7490e
lucio <lucio0314@163.com>
2017-07-31 12:38:36 +0800

    commit

--- src/main/java/com/mvvm/lux/framework/manager/imageloader/MyGlideModule.java
+++ src/main/java/com/mvvm/lux/framework/manager/imageloader/MyGlideModule.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.GlideModule;
@@ -18,6 +19,7 @@ import com.bumptech.glide.module.GlideModule;
 public class MyGlideModule implements GlideModule {
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setDecodeFormat(DecodeFormat.PREFER_RGB_565);
         // Apply options to the builder here.
         int maxMemory = (int) Runtime.getRuntime().maxMemory();//获取系统分配给应用的总内存大小
         int memoryCacheSize = maxMemory / 8;//设置图片内存缓存占用八分之一
